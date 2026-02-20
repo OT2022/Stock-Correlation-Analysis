8e6730fe386248229206135f82b0d43ec0c362b5
OT2022 <106277983+OT2022@users.noreply.github.com>
2026-02-20 23:10:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,21 @@ import pandas as pd
 import yfinance as yf
 
 st.title("Relative Value: Correlation Tool")
-st.caption("Note: Only UK (.L) tickers are currently supported. More countries to be added soon.")
+st.caption("Note: Only UK (.L) and US (.US) tickers are currently supported. More countries to be added soon.")
 
 # User input
-ticker = st.text_input("Enter Ticker e.g. VOD.L, BP.L, LLOY.L", "VOD.L")
+ticker = st.text_input("Enter Ticker e.g. VOD.L, BP.L, LLOY.L, AMZN.US", "VOD.L")
 start_date = st.text_input("Enter start date e.g. 2025-01-01", "2025-01-01")
 end_date = st.text_input("Enter end date e.g. 2025-12-31", "2025-12-31")
 
 # Map ticker to index
 if ".L" in ticker:
     index = '^FTSE'
+elif ".US" in ticker:
+    index = '^GSPC'
 else:
-    st.error("Ticker is not currently available")
-    st.stop() 
+    st.error("Ticker is not currently available. Please use a ticker ending in '.L' for UK stocks or '.US' for US stocks.")
+    st.stop()
 
 # Calculation logic
 # We use an 'if' statement for the button
@@ -23,27 +25,22 @@ if st.button('Calculate Correlation'):
     # Download data using the variables
     data = yf.download([ticker, index], start=start_date, end=end_date)
     data_adj = data['Close']
-    
+
     # Check if data was actually returned
     if not data_adj.empty:
         # 1. Calculate the Rebased series (Start at 100)
         rebased_data = (data_adj / data_adj.iloc[0]) * 1
         # Calculate returns
         price_return = data_adj.pct_change().dropna()
-        
+
         # Calculate correlation
         correlation = price_return[ticker].corr(price_return[index])
 
         # User output
         st.metric(label=f"Correlation: {ticker} vs {index}", value=f"{correlation:.2f}")
-        
+
         # Display the chart
         st.subheader("Relative Performance")
         st.line_chart(rebased_data)
     else:
         st.error("No data found for those dates or tickers.")
-
-
-
-
-                       
